b5bbf014450a76fc9e5d4085683dc402b4fd39e3
MJG <MJG@MacBook-Pro.local>
2016-04-22 13:34:36 -0400

    item count working

--- js/controllers/todo_ctrl.js
+++ js/controllers/todo_ctrl.js
@@ -9,6 +9,16 @@
 
   function ToDoItemsController(toDoFactoryObj) {
 
+    this.itemsLeft = function itemsLeft() {
+      var itemsRemaining = 0;
+      this.itemList.forEach(function countRemaining(each) {
+        if(!each.completed){
+          itemsRemaining++;
+        }
+      });
+      return itemsRemaining;
+    };
+
     this.itemList = toDoFactoryObj.itemList;
 
     this.newItem = {
@@ -24,6 +34,16 @@
         completed: false,
         editing: false
       };
+    };
+
+    this.keyPress = function keyPress(keyCode, item) {
+      if (keyCode === 13 || keyCode === 27) {
+        item.editing = !item.editing;
+      }
+    };
+
+    this.deleteItem = function deleteItem(indexNumber) {
+      this.itemList.splice(indexNumber, 1);
     };
 
   }
